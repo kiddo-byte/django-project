9e142cb0c4ae503c2b478d57457d2f51da63fa07
emobilis <seanorina@gmail.com>
2023-02-27 10:20:00 +0300

    django forms

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.reg, name='register'),
+    path('members/', views.reg_members, name='members'),
 
 ]
